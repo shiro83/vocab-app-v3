427e2681c55aa3e6a6c2188ac3dbb9e3347dc1a0
shirotori <pengin01@gmail.com>
2025-07-25 20:31:57 +0900

    途中

---
+++ composables/WordsForm.vue
@@ -0,0 +1,12 @@
+<!-- components/WordsForm.vue -->
+<template>
+  <form>
+    <input v-model="word" placeholder="単語を入力" />
+    <button type="submit">追加</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const word = ref("");
+</script>
